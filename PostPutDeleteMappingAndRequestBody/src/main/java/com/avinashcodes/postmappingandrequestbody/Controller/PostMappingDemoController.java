5ff2a1f1c49dd2aca898f87b5c74180f7fa8fc1f
Avinash Kumar <avinash.kumar93801@gmail.com>
2023-07-09 23:36:50 +0530

    Fixed error in class name in HTTP mapping demo.

--- PostPutDeleteMappingAndRequestBody/src/main/java/com/avinashcodes/postmappingandrequestbody/Controller/PostMappingDemoController.java
+++ PostPutDeleteMappingAndRequestBody/src/main/java/com/avinashcodes/postmappingandrequestbody/Controller/PostMappingDemoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DemoController {
+public class PostMappingDemoController {
 
     @PostMapping("/student/create")
     public Student createStudent(@RequestBody Student student){
